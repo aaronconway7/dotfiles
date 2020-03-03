c9bf4a1650bd37726597e598d0c1362079d7f9b5
aaronconway7 <aaronconway796@gmail.com>
2020-03-03 15:31:39 +0000

    updated dot files 3/3/2020

--- .hyper.js
+++ .hyper.js
@@ -4,10 +4,9 @@
 
 module.exports = {
     config: {
-        
         // choose either `'stable'` for receiving highly polished,
         // or `'canary'` for less polished but more frequent updates
-        updateChannel: "stable",
+        updateChannel: 'stable',
 
         // default font size in pixels for all tabs
         fontSize: 12,
@@ -16,81 +15,92 @@ module.exports = {
         fontFamily: '"Fira Code", Menlo, "DejaVu Sans Mono", Consolas, "Lucida Console", monospace',
 
         // default font weight: 'normal' or 'bold'
-        fontWeight: "bold",
+        fontWeight: 'bold',
 
         // font weight for bold characters: 'normal' or 'bold'
-        fontWeightBold: "bold",
+        fontWeightBold: 'bold',
+
+        // line height as a relative unit
+        lineHeight: 1,
+
+        // letter spacing as a relative unit
+        letterSpacing: 0,
 
         // terminal cursor background color and opacity (hex, rgb, hsl, hsv, hwb or cmyk)
-        cursorColor: "rgba(248,28,229,0.8)",
+        cursorColor: 'rgba(248,28,229,0.8)',
 
         // terminal text color under BLOCK cursor
-        cursorAccentColor: "#000",
+        cursorAccentColor: '#000',
 
         // `'BEAM'` for |, `'UNDERLINE'` for _, `'BLOCK'` for █
-        cursorShape: "BLOCK",
+        cursorShape: 'BLOCK',
 
         // set to `true` (without backticks and without quotes) for blinking cursor
         cursorBlink: true,
 
         // color of the text
-        foregroundColor: "#fff",
+        foregroundColor: '#fff',
 
         // terminal background color
         // opacity is only supported on macOS
-        backgroundColor: "rgba(0,0,0,0.95)",
-        // backgroundColor: "rgba(38, 51, 65, 1.000)",
+        backgroundColor: '#000',
 
         // terminal selection color
-        selectionColor: "rgba(248,28,229,0.3)",
+        selectionColor: 'rgba(248,28,229,0.3)',
 
         // border color (window, tabs)
-        borderColor: "#333",
+        borderColor: '#333',
 
         // custom CSS to embed in the main window
-        css: `
-            .tabs_title .tab_process.process_shell:before,
-            .tab_tab.tab_active .tab_process.process_shell:before,
-            .tab_tab:hover .tab_process.process_shell:before {
-                background-color: #fffa72;
-            }
-        `,
+        css: '',
 
         // custom CSS to embed in the terminal window
-        termCSS: "",
+        termCSS: '',
 
         // if you're using a Linux setup which show native menus, set to false
         // default: `true` on Linux, `true` on Windows, ignored on macOS
-        showHamburgerMenu: "",
+        showHamburgerMenu: '',
 
         // set to `false` (without backticks and without quotes) if you want to hide the minimize, maximize and close buttons
         // additionally, set to `'left'` if you want them on the left, like in Ubuntu
         // default: `true` (without backticks and without quotes) on Windows and Linux, ignored on macOS
-        showWindowControls: "",
+        showWindowControls: '',
 
         // custom padding (CSS format, i.e.: `top right bottom left`)
-        padding: "12px 14px 50px 14px",
+        padding: '12px 14px',
 
         // the full list. if you're going to provide the full color palette,
         // including the 6 x 6 color cubes and the grayscale map, just provide
         // an array here instead of a color map object
         colors: {
-            black: "#000000",
-            red: "#C51E14",
-            green: "#1DC121",
-            yellow: "#C7C329",
-            blue: "#0A2FC4",
-            magenta: "#C839C5",
-            cyan: "#20C5C6",
-            white: "#C7C7C7",
-            lightBlack: "#686868",
-            lightRed: "#FD6F6B",
-            lightGreen: "#67F86F",
-            lightYellow: "#FFFA72",
-            lightBlue: "#6A76FB",
-            lightMagenta: "#FD7CFC",
-            lightCyan: "#68FDFE",
-            lightWhite: "#FFFFFF"
+            black: '#000000',
+            red: '#C51E14',
+            green: '#1DC121',
+            yellow: '#C7C329',
+            blue: '#0A2FC4',
+            magenta: '#C839C5',
+            cyan: '#20C5C6',
+            white: '#C7C7C7',
+            lightBlack: '#686868',
+            lightRed: '#FD6F6B',
+            lightGreen: '#67F86F',
+            lightYellow: '#FFFA72',
+            lightBlue: '#6A76FB',
+            lightMagenta: '#FD7CFC',
+            lightCyan: '#68FDFE',
+            lightWhite: '#FFFFFF',
+        },
+
+        // hyperborder config
+        hyperBorder: {
+            borderRadiusInner: "5px",
+            borderRadiusOuter: "5px",
+            animate: true
+        },
+
+        hyperTabs: {
+            border: true,
+            tabIconsColored: true,
         },
 
         // the shell to run when spawning a new session (i.e. /usr/local/bin/fish)
@@ -105,17 +115,17 @@ module.exports = {
         //
         // PowerShell on Windows
         // - Example: `C:\\WINDOWS\\System32\\WindowsPowerShell\\v1.0\\powershell.exe`
-        shell: "zsh",
+        shell: '/bin/zsh',
 
         // for setting shell arguments (i.e. for using interactive shellArgs: `['-i']`)
         // by default `['--login']` will be used
-        shellArgs: ["--login"],
+        shellArgs: ['--login'],
 
         // for environment variables
         env: {},
 
         // set to `false` for no bell
-        bell: "SOUND",
+        bell: 'SOUND',
 
         // if `true` (without backticks and without quotes), selected text will automatically be copied to the clipboard
         copyOnSelect: false,
@@ -125,45 +135,21 @@ module.exports = {
 
         // if `true` (without backticks and without quotes), on right click selected text will be copied or pasted if no
         // selection is present (`true` by default on Windows and disables the context menu feature)
-        // quickEdit: true,
+        quickEdit: false,
+
+        // choose either `'vertical'`, if you want the column mode when Option key is hold during selection (Default)
+        // or `'force'`, if you want to force selection regardless of whether the terminal is in mouse events mode
+        // (inside tmux or vim with mouse mode enabled for example).
+        macOptionSelectionMode: 'vertical',
 
         // URL to custom bell
         // bellSoundURL: 'http://example.com/bell.mp3',
 
+        // Whether to use the WebGL renderer. Set it to false to use canvas-based
+        // rendering (slower, but supports transparent backgrounds)
+        webGLRenderer: false,
+
         // for advanced config flags please refer to https://hyper.is/#cfg
-
-        verminal: {
-            fontFamily: '"Fira Code"',
-            fontSize: 12,
-            fontWeight: "bold",
-        },
-
-        hyperBorder: {
-            borderRadiusInner: "5px",
-            borderRadiusOuter: "5px",
-            animate: true
-        },
-
-        hyperline: {                                                                 
-            plugins: [                                                    
-                "ip",                                                                    
-                "cpu",
-                "network",                                                                   
-                "spotify",
-                "battery"                                                              
-            ]                                                                          
-        },
-
-        hyperTabs: {
-            border: true,
-            tabIconsColored: true,
-        },
-
-        hyperTransparent: {
-            // backgroundColor: '#4b4',
-            opacity: 0.2,
-            vibrancy: 'dark' // ['', 'dark', 'medium-light', 'ultra-dark']
-        }
     },
 
     // a list of plugins to fetch and install from npm
@@ -173,18 +159,15 @@ module.exports = {
     //   `@company/project`
     //   `project#1.0.1`
     plugins: [
-        "verminal",
+        "hyper-snazzy",
+        // "hyperborder",
         "hypercwd",
         "hyperterm-tabs",
-        "hyperlinks",
-        //   "hyperborder",
-        //   "hyper-tab-icons",
-        "hyperpower",
-        "hyperline",
-        //   "hyper-search",
-        //   'hyper-statusline',
-        "hyper-tabs-enhanced",
-        "hyper-transparent"
+        "hyper-statusline",
+        // "hyper-search",
+        // "hyperpower"
+        "hyper-tab-icons",
+        "hyper-font-ligatures"
     ],
 
     // in development, you can create a directory under
@@ -195,5 +178,5 @@ module.exports = {
     keymaps: {
         // Example
         // 'window:devtools': 'cmd+alt+o',
-    }
+    },
 };
